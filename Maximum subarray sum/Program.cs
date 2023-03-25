886ba317359686ab530d7b06dad4fdb10893ffd7
AHX <vladis9484@gmail.com>
2023-03-25 14:17:46 +0300

    Added Maximum_subarray_sum, TwoSum

---
+++ Maximum subarray sum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Maximum_subarray_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxSequence(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        }
+        public static int MaxSequence(int[] arr)
+        {
+            int sum = 0;
+            int maxSum = 0;
+            foreach (int i in arr)
+            {
+                if(sum < 0) { sum = 0; }
+                sum += i;
+                if(sum > maxSum) { maxSum = sum; }
+            }
+            return maxSum;
+        }
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TwoSum(new[] { 1, 2, 3 }, 4).OrderBy(a => a).ToArray());
+        }
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if (numbers[i] + numbers[j] == target)
+                        return new int[] {i, j};
+                }
+            }
+            return new int[0];
+        }
+    }
+}
